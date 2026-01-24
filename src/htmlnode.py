d156226b5caf6df3aace0206040673f2803fe0a2
Ab-dell <hakemabdel59@gmail.com>
2026-01-24 16:48:12 +0100

    creat an HtmlNode class that will be used to convert makdown text to actual html tags

---
+++ src/htmlnode.py
@@ -0,0 +1,29 @@
+
+
+
+
+
+class HtmlNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def __repr__(self):
+        return f"HtmlNode(tag:{self.tag}, value:{self.value}, children:{self.children}, attributes:{self.props})"
+
+
+    def to_html(self):
+        raise(NotImplementedError)
+    
+
+    def props_to_html(self):
+        if self.props == None:
+            return ""
+        
+        formated_string = f""
+        for prop in self.props:
+            formated_string += f" {prop}={self.props[prop]}"
+
+        return formated_string
