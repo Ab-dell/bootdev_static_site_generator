d64cbd406a52762eb0988adfb461d6e6b6a77937
Ab-dell <hakemabdel59@gmail.com>
2026-01-25 14:26:02 +0100

    move leafnode class into htmlnode.py so every class that inherit from htmlnode will be in the same file

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -27,3 +27,25 @@ class HtmlNode:
             formated_string += f' {prop}="{self.props[prop]}"'
 
         return formated_string
+    
+
+
+class LeafNode(HtmlNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def props_to_html(self):
+        return super().props_to_html()
+
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("All leaf nodes must have values")
+        
+        if self.tag == None:
+            return self.value
+        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"HtmlNode(tag:{self.tag}, value:{self.value}, attributes:{self.props})"
